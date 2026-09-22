096e74cae63c060184d13ff3a4d32e0432a43429
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-22 19:48:46 +0100

    Applied ESLint code fixes

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,6 @@
 import {expect, test} from 'bun:test'
 
 const {default: decodeBrotli} = await import('#src/main.ts')
-
 test('should run', () => {
   const result = decodeBrotli()
   expect(result).toBe('decode-brotli') // TODO Test actual functionality
